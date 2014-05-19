a20e1d6a6e2251323e199cacfec21f23e879f455
Emil Virkki <emil.virkki@gmail.com>
2014-05-19 22:02:44 +0300

    Prevented user scaling

--- tpl.php
+++ tpl.php
@@ -9,7 +9,7 @@ function head($product_name, $title = '', $tab = 1) {
 <head>
 	<meta charset="utf-8">
 	<title><?php print $title; ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 	<link rel="stylesheet" href="style/style.css">
 </head>
 <body>
